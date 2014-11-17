ebe6e8217cfe5eff365784839ea7a22ad97ae788
Nathan Stitt <nathan@stitt.org>
2014-11-16 20:49:19 -0600

    Better help messages

--- lib/lanes/generators/generate.rb
+++ lib/lanes/generators/generate.rb
@@ -5,7 +5,7 @@ module Lanes
 
             desc 'generate model [NAME]', 'Creates or update a model'
             long_desc Pathname.new(__FILE__).dirname.join("generate_model.usage").read
-            register Generators::Model,  'model',      'model [NAME] ...FIELDS', 'Creates a new model'
+            register Generators::Model, 'model', 'model [NAME] ...FIELDS', 'Creates a new model'
 
         end
 
