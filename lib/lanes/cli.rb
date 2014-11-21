c6e6dec0115320352a54c06872270aa7da9fe4bc
Nathan Stitt <nathan@stitt.org>
2014-11-20 22:20:41 -0600

    New lanes update command for updating client model

--- lib/lanes/cli.rb
+++ lib/lanes/cli.rb
@@ -1,17 +1,31 @@
 require 'thor'
+require 'require_all'
 require_relative '../lanes'
-require_relative 'command/app'
-require_relative 'command/model'
-require_relative 'command/generate'
+require_relative 'command'
+require_rel 'command/*.rb'
 
 module Lanes
 
+    # This is the main interface to Lanes that is called by the command
+    # `bin/lanes`. Do not put any logic in here, create a class and delegate
+    # instead.
     class CLI < Thor
 
-        register Command::App,      'new',      'new [NAME]', 'Creates a new Lanes based application'
+        register Command::App, 'new', 'new [NAME]', 'Creates a new Lanes based application'
 
         desc "generate SUBCOMMAND ...ARGS", "Generate a component"
         subcommand "generate", Command::Generate
+
+        desc "update SUBCOMMAND ...ARGS", "Update a component"
+        subcommand "update", Command::Update
+
+        long_desc Command.usage_from_file("server")
+        register Command::Server,  'serve', 'serve', 'Run the app in development/testing mode'
+
+        long_desc Command.usage_from_file("db")
+        register Command::Db,  'db', 'db', 'Database commands'
+
+        register Command::Console, "console",  "console", "Start IRB console"
 
     end
 
