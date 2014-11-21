c719a079b64c0d1920a41a154021bb312612e0fa
Nathan Stitt <nathan@stitt.org>
2014-11-20 22:22:41 -0600

    db tasks are now part of the lanes command

--- lib/lanes/db.rb
+++ lib/lanes/db.rb
@@ -1,5 +1,3 @@
-require_relative 'db/seed'
-
 module Lanes
 
     module DB
@@ -18,21 +16,17 @@ module Lanes
             ::ActiveRecord::Base.establish_connection( configuration )
         end
 
-        # def migration_exists?( file_name ) #:nodoc:
-        #     Dir.glob("#{migrations_dir}/[0-9]*_*.rb").grep(/\d+_#{file_name}.rb$/).first
-        # end
-
         def load_seed
-            load Pathname.new(__FILE__).dirname.join("../../db/seed.rb")
-            Seed.execute!
+            path = Pathname.new("./db/seed.rb")
+            load path if path.exist?
         end
 
         def configure_rake_environment
             ActiveRecord::Tasks::DatabaseTasks.seed_loader = Lanes::DB
             env = ENV['RAILS_ENV'] || 'development'
-            Lanes::DB.config_file ||= 'config/database.yml'
-            ENV['SCHEMA'] ||= 'db/schema.sql'
-            ENV['DB_STRUCTURE'] ||= 'db/schema.sql'
+            Lanes::DB.config_file  ||= 'config/database.yml'
+            ENV['SCHEMA']          ||= 'db/schema.sql'
+            ENV['DB_STRUCTURE']    ||= 'db/schema.sql'
             ActiveRecord::Base.schema_format = :sql
             Lanes::DB.establish_connection( env )
             ActiveRecord::Tasks::DatabaseTasks.database_configuration = ActiveRecord::Base.configurations
@@ -41,40 +35,6 @@ module Lanes
             ActiveRecord::Tasks::DatabaseTasks.current_config( :config => ActiveRecord::Base.configurations[ env ] )
         end
 
-        # def create_migration( migration_name, fields={} )
-        #     STDERR.puts "Migration #{migration_name} already exists!" and return false if migration_exists?( migration_name )
-        #     migration_number = Time.now.utc.strftime("%Y%m%d%H%M%S")
-        #     migration_file = File.join(migrations_dir, "#{migration_number}_#{migration_name}.rb")
-        #     migration_class = migration_name.split("_").map(&:capitalize).join
-        #     File.open(migration_file, 'w') do |file|
-        #         migration = "class #{migration_class} < ActiveRecord::Migration\n    def change\n"
-        #         fields.each do |name, type|
-        #             migration << "        " +
-        #         end
-        #         file.write migration
-        #         <<-MIGRATION.strip_heredoc
-
-        #             MIGRATION
-        #             <<-MIGRATION.strip_heredoc
-        #                 end
-        #             end
-        #             MIGRATION
-        #     end
-        # end
-
-        def migrate(version = nil)
-            silence_activerecord do
-                migration_version = version ? version.to_i : version
-                ::ActiveRecord::Migrator.migrate( migrations_dir, migration_version )
-            end
-        end
-
-        def rollback(step = nil)
-            silence_activerecord do
-                migration_step = step ? step.to_i : 1
-                ::ActiveRecord::Migrator.rollback(migrations_dir, migration_step)
-            end
-        end
 
         private
 
