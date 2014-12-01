47b55c70760d8066965a71102740dd4d1f2fa5f3
Nathan Stitt <nathan@stitt.org>
2014-11-30 21:18:38 -0600

    require_tree vs individual files

--- client/workspace/index.js
+++ client/workspace/index.js
@@ -1,14 +1,4 @@
-//= require ../lib/noConflict
-//= require ../vendor/modern-stack
-//= require ../lib
 //= require_self
-//= require ../extension
-//= require ../data
-//= require ../plugins
-//= require ../views
-//= require ../screens
-//= require ../component
 //= require_tree .
-//= require ../extension/LateLoaded
 
 Lanes.namespace( 'Workspace' );
