7ff0899b37ff5c12571dd53c58caad715c8d3a72
Nathan Stitt <nathan@stitt.org>
2014-11-30 21:16:11 -0600

    New npm build

--- npm-build/template.js
+++ npm-build/template.js
@@ -3,7 +3,7 @@ var Lanes = ( global.Lanes || (global.Lanes = {}) );
 Lanes.Vendor = ( Lanes.Vendor || {} );
 
 Lanes.Vendor.Ampersand                = ( Lanes.Ampersand || {} );
-Lanes.Vendor.Ampersand.View           = require("ampersand-view");
+Lanes.Vendor.Ampersand.Bindings       = require("ampersand-dom-bindings");
 Lanes.Vendor.Ampersand.State          = require("ampersand-state");
 Lanes.Vendor.Ampersand.CollectionView = require("ampersand-collection-view");
 Lanes.Vendor.Ampersand.SubCollection  = require("ampersand-subcollection");
@@ -12,17 +12,17 @@ Lanes.Vendor.Ampersand.USCollection   = require('ampersand-collection-underscore
 Lanes.Vendor.Ampersand.Collection     = require("ampersand-collection");
 Lanes.Vendor.Ampersand.Model          = require("ampersand-model");
 Lanes.Vendor.Ampersand.Router         = require("ampersand-router");
-Lanes.Vendor.AmpersandFormView        = require("ampersand-form-view");
-Lanes.Vendor.AmpersandInputView       = require("ampersand-input-view");
 Lanes.Vendor.KeyMaster                = require("keymaster");
 Lanes.Vendor.Moment                   = require("moment");
 Lanes.Vendor.RSVP                     = require('rsvp');
+Lanes.Vendor.domify                   = require('domify');
+Lanes.Vendor.BBEvents                 = require('backbone-events-standalone');
 
 var u        = require('underscore');
 var spf      = require('sprintf-js');
 
-u.Promise    = Lanes.Vendor.RSVP.Promise;
-u.defer      = Lanes.Vendor.RSVP.defer;
+u.Promise         = Lanes.Vendor.RSVP.Promise;
+u.DeferredPromise = Lanes.Vendor.RSVP.defer;
 
 u.getPath    = require('get-object-path');
 u.bigDecimal = require('big.js');
