5e1607dc249aaf64103052a965c66ce2719d3bad
Nathan Stitt <nathan@stitt.org>
2014-11-30 21:03:22 -0600

    add extensions routes

--- lib/lanes/api/routing.rb
+++ lib/lanes/api/routing.rb
@@ -16,8 +16,22 @@ module Lanes
             @routes ||= RouteSet.new(API::Root)
         end
 
-        Extensions.each do | ext |
-            ext.route(RouteSet.new(API::Root))
+        self.routes.draw do
+            get '/' do
+                content_type 'text/html'
+                erb :index
+            end
+
+            get "default-records" do
+                { success: true, data: Lanes::API.default_records }
+            end
+
+            Extensions.each do | ext |
+                ext.route(self)
+            end
+
         end
+
+
     end
 end
