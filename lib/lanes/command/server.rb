23fb7445c6f33f26f157faa93acfdd6419ded218
Nathan Stitt <nathan@stitt.org>
2014-11-30 21:09:36 -0600

    output help message with access url

--- lib/lanes/command/server.rb
+++ lib/lanes/command/server.rb
@@ -9,6 +9,7 @@ module Lanes
             class_option  :port, type: :numeric
 
             def launch
+                say "Launching testing server at http://localhost:8888/", :green
                 Guard.start
             end
         end
