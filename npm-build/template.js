adb5e633411cf21fbc7d298d74ace66638b61522
Nathan Stitt <nathan@stitt.org>
2014-12-05 20:32:03 -0600

    Mixin utility functions onto underscore

--- npm-build/template.js
+++ npm-build/template.js
@@ -17,17 +17,17 @@ Lanes.Vendor.Moment                   = require("moment");
 Lanes.Vendor.RSVP                     = require('rsvp');
 Lanes.Vendor.domify                   = require('domify');
 Lanes.Vendor.BBEvents                 = require('backbone-events-standalone');
-
+Lanes.dom    = require('ampersand-dom');
 var u        = require('underscore');
 var spf      = require('sprintf-js');
 
-u.Promise         = Lanes.Vendor.RSVP.Promise;
-u.DeferredPromise = Lanes.Vendor.RSVP.defer;
-
-u.getPath    = require('get-object-path');
-u.bigDecimal = require('big.js');
-u.dom        = require('ampersand-dom');
-u.sprintf    = spf.sprintf;
-u.vsprintf   = spf.vsprintf;
-
+u.mixin(require('underscore.inflections'));
+u.mixin({
+    Promise         : Lanes.Vendor.RSVP.Promise,
+    DeferredPromise : Lanes.Vendor.RSVP.defer,
+    getPath         : require('get-object-path'),
+    bigDecimal      : require('big.js'),
+    sprintf         : spf.sprintf,
+    vsprintf        : spf.vsprintf
+});
 global._ = u;
