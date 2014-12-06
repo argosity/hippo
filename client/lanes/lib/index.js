cf2608753c4514dc22c5ba8389da47b623af1e74
Nathan Stitt <nathan@stitt.org>
2014-12-05 21:01:22 -0600

    don't require underscore

--- client/lanes/lib/index.js
+++ client/lanes/lib/index.js
@@ -1,9 +1,5 @@
 //= require ./noConflict
 //= require ./namespace
 //= require ./utilFunctions
-//= require ./underscore.inflection
 //= require ./ModuleSupport
 //= require_tree .
-
-
-Lanes.namespace('lib');
