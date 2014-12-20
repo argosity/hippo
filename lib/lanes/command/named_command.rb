88e70daeec0bf6910552bcdd9ea108aa9872c84e
Nathan Stitt <nathan@stitt.org>
2014-12-20 16:36:41 -0600

    indentation

--- lib/lanes/command/named_command.rb
+++ lib/lanes/command/named_command.rb
@@ -15,7 +15,7 @@ module Lanes
             def load_namespace
                 @namespace  = options[:namespace] ||
                   Command.load_current_extension(raise_on_fail:true).identifier
-              end
+            end
 
             def set_variables
                 @class_name = name.classify
