a0747d3c7c74ba74224a79a7eb76b50000d4db76
Nathan Stitt <nathan@stitt.org>
2015-01-19 21:57:39 -0600

    Remove helpers left over from Stockor

--- lib/lanes/spec_helper.rb
+++ lib/lanes/spec_helper.rb
@@ -113,6 +113,7 @@ module MiniTest
         def assert_saves( model )
             assert model.save, "#{model.class} failed to save: #{model.errors.full_messages.join(',')}"
         end
+
         def refute_saves( model, *errors )
             refute model.save, "#{model.class} saved successfully when it should not have"
             errors.each do |error|
@@ -122,7 +123,6 @@ module MiniTest
             end
 
         end
-
 
         def assert_event_fires( klass, event, &block )
             @event_results = []
@@ -138,31 +138,3 @@ module MiniTest
         end
     end
 end
-
-
-module FixtureHelpers
-
-    extend ActiveSupport::Concern
-
-    included do
-        alias_method_chain :table_rows, :custom_autoset_fields
-    end
-
-    def table_rows_with_custom_autoset_fields
-        results = table_rows_without_custom_autoset_fields
-        if model_class
-            results[ table_name ].each do | row |
-                row['visible_id'] = Lanes::SequentialId.next_for( model_class ) if model_class.column_names.include?('visible_id')
-                row['hash_code' ] = Lanes::Strings.random if model_class.column_names.include?('hash_code')
-                row['created_at'] = Time.now if model_class.column_names.include?('created_at')
-                row['updated_at'] = Time.now if model_class.column_names.include?('updated_at')
-                # 135138680 == "admin"
-                row['created_by_id'] = 135138680  if model_class.column_names.include?('created_by_id')
-                row['updated_by_id'] = 135138680 if model_class.column_names.include?('updated_by_id')
-            end
-        end
-        results
-    end
-end
-
-ActiveRecord::FixtureSet.send :include, FixtureHelpers
