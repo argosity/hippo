3c8c93118156d6a429d849921877f8545d2e5308
Nathan Stitt <nathan@stitt.org>
2015-01-24 21:46:09 -0600

    Log progress during db commands

--- lib/lanes/command/db.rb
+++ lib/lanes/command/db.rb
@@ -21,8 +21,10 @@ module Lanes
             def run_task(task)
                 Command.load_current_extension
                 require_relative "../rake_tasks"
+                say("Executing " + task)
                 Extensions.each do |ext|
                     Dir.chdir(ext.root_path) do
+                        say("\t" + ext.identifier)
                         Rake::Task[task].reenable
                         Rake::Task[task].invoke
                     end
