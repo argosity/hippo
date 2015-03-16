f6c0df374a61dd0528f0aeb30e1b8ba92a627f37
Nathan Stitt <nathan@stitt.org>
2015-03-15 19:12:33 -0500

    Set options for minitest

--- lib/lanes/guard_tasks.rb
+++ lib/lanes/guard_tasks.rb
@@ -26,15 +26,19 @@ module Lanes
                 spec_dir: "spec/#{app_name}", console: :always, debug: false
             })
 
+            minitest_options = {
+              all_on_start: true, test_folders: 'spec/server'
+            }
+
             dsl.guard :jasmine, jasmine_options do
                 dsl.watch(%r{^client/(.+?)\.(js|coffee)$}){ |m| "spec/#{m[1]}Spec.#{m[2]}" }
                 dsl.watch(%r{^spec/.*(?:_s|S)pec\.coffee$})
                 matchers.client_matches.call if matchers.client_matches
             end
 
-            dsl.guard :minitest, :all_on_start => true do
-                dsl.watch(%r{^spec/spec_helper\.rb}) { 'test' }
-                dsl.watch(%r{^spec/.+_spec\.rb})
+            dsl.guard :minitest, minitest_options do
+                dsl.watch(%r{^spec/server/spec_helper\.rb}) { 'test' }
+                dsl.watch(%r{^spec/server/.+_spec\.rb})
                 dsl.watch(%r{^spec/fixtures/#{app_name}/(.+)s\.yml})   { |m| "spec/server/#{m[1]}_spec.rb" }
                 dsl.watch(%r{^lib/#{app_name}/(.+)\.rb})               { |m| "spec/server/#{m[1]}_spec.rb" }
                 matchers.server_matches.call if matchers.server_matches
