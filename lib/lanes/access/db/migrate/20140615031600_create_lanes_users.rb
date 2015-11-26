4c5ccf558a87f1c3b1f11e1e080e0c408e4a3152
Nathan Stitt <nathan@stitt.org>
2015-11-25 21:22:26 -0600

    hstore -> jsonb

--- lib/lanes/access/db/migrate/20140615031600_create_lanes_users.rb
+++ lib/lanes/access/db/migrate/20140615031600_create_lanes_users.rb
@@ -4,7 +4,7 @@ class CreateLanesUsers < ActiveRecord::Migration
             t.string :login, :name, :email,  null: false
             t.string :password_digest, null: false
             t.string  "role_names",   array: true, null:false, default: []
-            t.hstore "options",       null: false, default:  {}
+            t.jsonb   "options",      null: false, default:  {}
             t.timestamps null:false
         end
         add_index  :lanes_users, :role_names, using: 'gin'
