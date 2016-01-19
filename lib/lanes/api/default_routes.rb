48e136a1ec22e22d9ff8d37cdd8ab9eb8a283fac
Nathan Stitt <nathan@stitt.org>
2016-01-18 22:23:54 -0600

    Routes for system settings and files

--- lib/lanes/api/default_routes.rb
+++ lib/lanes/api/default_routes.rb
@@ -1,8 +1,14 @@
 module Lanes
     API.routes.draw do
-        get "default-records" do
-            { success: true, data: Lanes::API.default_records }
-        end
+
+        patch Lanes.config.mounted_at + '/system-settings.json',
+              &SystemSettings.update_handler
+
+        post Lanes.config.mounted_at + '/save-file-attribute',
+             &API::Handlers::File.saver
+
+        get Lanes.config.mounted_at + '/file/*',
+            &API::Handlers::File.getter
 
         Extensions.each do | ext |
             ext.route(self)
