9638172ebf656409613df191590559f4e2adce5e
Nathan Stitt <nathan@stitt.org>
2016-01-31 11:32:39 -0600

    Work around es7 exprots from new version

--- npm-build/development.js
+++ npm-build/development.js
@@ -1,6 +1,6 @@
 var Lanes = ( global.Lanes || (global.Lanes = {}) );
 Lanes.Vendor = ( Lanes.Vendor || {} );
 
-Lanes.Vendor.ReactProxy      = require("react-proxy");
+Lanes.Vendor.ReactProxy      = require("react-proxy").default;
 Lanes.Vendor.ReactTestUtils  = require('react-addons-test-utils');
 Lanes.Vendor.deepForceUpdate = require('react-deep-force-update');
