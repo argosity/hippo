99e29b71c6e246a4ef3fa5591d2714f040932176
Nathan Stitt <nathan@stitt.org>
2016-04-24 21:18:25 -0500

    Check user is logged in and has access

--- lib/lanes/api/handlers/file.rb
+++ lib/lanes/api/handlers/file.rb
@@ -7,19 +7,21 @@ module Lanes::API::Handlers
                 Lanes.logger.debug "Saving File. Root=#{CarrierWave.root}"
                 path = "#{params['extension_id']}/#{params['type']}"
                 model = path.underscore.camelize.constantize
-                record = model.find(params['id'])
-                params['files'].each do |attr, data|
-                    record.send(attr).store!( data )
+                authentication = Lanes::API::AuthenticationProvider.new(request)
+                authentication.wrap_model_access(model, self) do
+                    record = model.find(params['id'])
+                    params['files'].each do |attr, data|
+                        record.send(attr).store!( data )
+                    end
+                    record.save!
+                    json_reply std_api_reply :create, { model: record }, success: true
                 end
-                record.save!
-
-                json_reply std_api_reply :create, { model: record }, success: true
             end
         end
 
         def self.getter
-            lambda do
-                send_file CarrierWave::Uploader::Base.root.call + '/' + params['splat'].first
+            Lanes::API::RequestWrapper.with_authenticated_user do |user, req|
+                req.send_file CarrierWave::Uploader::Base.root.call + '/' + req.params['splat'].first
             end
         end
     end
