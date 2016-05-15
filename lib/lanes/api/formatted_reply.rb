2593181b5f6d106c7672d7adda8a7960154b6e60
Nathan Stitt <nathan@stitt.org>
2016-05-14 23:24:10 -0500

    record errors as method for external use

--- lib/lanes/api/formatted_reply.rb
+++ lib/lanes/api/formatted_reply.rb
@@ -6,23 +6,27 @@ module Lanes
             # populates them appropriately
 
             def std_api_reply(type, data, options)
-                success = options[:success].nil? ? true : options[:success]
-                json = {}
-                if data.is_a?(ActiveRecord::Base) && data.errors.any?
-                    json[:errors] = {}
-                    success = false
-                    data.errors.each{ | attr, message |
-                        json[:errors][attr] = message
-                    }
+                json = { success: options[:success].nil? ? true : options[:success] }
+                if data.is_a?(ActiveRecord::Base)
+                    record_active_record_errors(data, json)
                 end
                 if options[:total_count]
                     json[:total] = options.delete(:total_count)
                 end
                 json.merge(
-                  success: success,
-                  message: options[:messsage] || json_status_str(data, type.to_s.capitalize, success),
-                  data:    success ? records_for_reply(data, type, options) : []
+                  message: options[:messsage] || json_status_str(data, type.to_s.capitalize, json[:success]),
+                  data:    json[:success] ? records_for_reply(data, type, options) : []
                 )
+            end
+
+            def record_active_record_errors(model, json = {})
+                return if model.errors.none?
+                json[:success] = false
+                json[:errors] = {}
+                model.errors.each{ | attr, message |
+                    json[:errors][attr] = message
+                }
+                json
             end
 
             # @return Array<Array> returns either an array of fields
