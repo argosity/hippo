4a7f0ee9bb16cab37c40c95de67f329ad5b2b2ee
Nathan Stitt <nathan@stitt.org>
2016-08-16 21:13:58 -0500

    only set created_by_id if not present

--- lib/lanes/access/test_fixture_extensions.rb
+++ lib/lanes/access/test_fixture_extensions.rb
@@ -9,8 +9,9 @@ module AccessFixtureTestPatches
         results = table_rows_without_custom_autoset_user_fields
         if model_class && model_class < ActiveRecord::Base && model_class.record_modifications
             results[ table_name ].each do | row |
-                row['created_by_id'] = 593363170 if model_class.column_names.include?('created_by_id')
-                row['updated_by_id'] = 593363170 if model_class.column_names.include?('updated_by_id')
+                # 135138680 is the 'admin' user
+                row['created_by_id'] ||= 135138680 if model_class.column_names.include?('created_by_id')
+                row['updated_by_id'] ||= 135138680 if model_class.column_names.include?('updated_by_id')
             end
         end
         results
