4e894fc4dfae0a7161d11d7668302f99929b1ce6
Nathan Stitt <nathan@stitt.org>
2016-08-28 21:54:17 -0500

    only select own columns if not already selected

--- lib/lanes/concerns/queries.rb
+++ lib/lanes/concerns/queries.rb
@@ -10,8 +10,12 @@ module Lanes
 
                 def compose_query_using_detail_view( view: nil, join_to: nil )
                     join_to ||= self.name.demodulize.tableize.singularize + '_' + primary_key
-                    joins("join #{view} as details on details.#{join_to} = #{table_name}.#{primary_key}")
-                        .select("#{table_name}.*, details.*")
+                    q = joins("join #{view} as details on details.#{join_to} = #{table_name}.#{primary_key}")
+                          .select("details.*")
+                    if current_scope.nil? || current_scope.select_values.exclude?("#{table_name}.*")
+                        q = q.select("#{table_name}.*")
+                    end
+                    q
                 end
 
             end
