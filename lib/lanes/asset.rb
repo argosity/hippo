8fb1bc73cd82341415fffb2b0fef9da55c779087
Nathan Stitt <nathan@stitt.org>
2016-11-03 22:25:49 -0500

    remove dead code

--- lib/lanes/asset.rb
+++ lib/lanes/asset.rb
@@ -8,31 +8,6 @@ module Lanes
 
         validates :owner,  set: true
 
-#        after_update :remove_changed_file, if: lambda{ file_changed? }
-
-        # def serializable_hash(options = nil)
-        #     values = super
-        #     values.delete('file')
-        #     values.merge!(file.as_json[:file].stringify_keys)
-        #     values['original'] = { 'url' => values.delete('url') }
-        #     values
-        # end
-
-        # def store_uploaded_file(f)
-        #     ext = File.extname(f[:filename])
-        #     if ext.blank?
-        #         ext = '.' + FastImage.type(f[:tempfile]).to_s
-        #     end
-        #     self[:file] = ::Lanes::Strings.random + ext
-        #     file.store!(f)
-        # end
-
-        # protected
-
-        # def remove_changed_file
-        #     self.file_was.remove!
-        # end
-
     end
 
 end
