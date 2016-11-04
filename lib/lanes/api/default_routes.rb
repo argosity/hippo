389932c8d2940bb4c9544495b123c24081f5b22a
Nathan Stitt <nathan@stitt.org>
2016-11-03 22:27:29 -0500

    re-arrange route order
    By moving them after extensions they can be overriden if needed

--- lib/lanes/api/default_routes.rb
+++ lib/lanes/api/default_routes.rb
@@ -1,5 +1,15 @@
+require_relative "handlers/asset.rb"
+
 module Lanes
     API.routes.draw do
+        # WS endpoint must come first
+        get Lanes.config.api_path + '/ws' do
+            API::Cable.server.call(env)
+        end
+
+        Extensions.each(reversed: true) do | ext |
+            ext.route(self)
+        end
 
         put Lanes.config.api_path + '/system-settings.json',
             &SystemSettings.update_handler
@@ -10,18 +20,9 @@ module Lanes
         get Lanes.config.api_path + '/asset/*',
             &API::Handlers::Asset.getter
 
-        get Lanes.config.api_path + '/ws' do
-            API::Cable.server.call(env)
-        end
-
         post Lanes.config.api_path + '/dev-file-change.json' do
             API::PubSub.publish("file-change", data)
             "OK"
         end
-
-        Extensions.each(reversed: true) do | ext |
-            ext.route(self)
-        end
-
     end
 end
