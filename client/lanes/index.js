37956892580c4b723a8b7b64adbf3c9190203017
Nathan Stitt <nathan@stitt.org>
2017-03-03 15:52:08 -0600

    remove asset_pipeline gunk

--- client/lanes/index.js
+++ client/lanes/index.js
@@ -1,12 +1,1 @@
-//= require ./lib
-//= require ./extension
-//= require ./models
-//= require ./Config
-//= require ./react
-//= require ./components
-//= require ./screens
-//= require ./extension/LateLoaded
-//= require ./Boot
-
-import Config from './config';
 import Boot from './boot';
