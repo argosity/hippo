b64e9e66107094e71825225f032d810e3ad7b364
Nathan Stitt <nathan@stitt.org>
2017-03-13 22:15:17 -0500

    render child

--- client/lanes/components/form-field.jsx
+++ client/lanes/components/form-field.jsx
@@ -2,47 +2,53 @@ import React from 'react';
 import { get } from 'lodash';
 import classnames from 'classnames';
 import { observer } from 'mobx-react';
-import { Col } from 'react-flexbox-grid';
+import { Col, getColumnProps } from 'react-flexbox-grid';
 import { titleize } from '../lib/util';
 import Field from 'grommet/components/FormField';
 import TextInput from 'grommet/components/TextInput';
 
 import './form-field.scss';
+import FieldValidation from './field-validation';
 
-export default class FormField extends React.Component {
+export default class FormField extends React.PureComponent {
     static defaultProps = {
         label:     '',
         className: '',
+        fields:    null,
     }
 
-    static propTypes = {
+    static propTypes = Object.assign({
         label: React.PropTypes.string,
-
-
         name:  React.PropTypes.string.isRequired,
-        field: React.PropTypes.shape({
-            events: React.PropTypes.object.isRequired,
-            value:  React.PropTypes.string.isRequired,
-        }).isRequired,
+        fields: FieldValidation,
         className: React.PropTypes.string,
-    }
+    }, Col.PropTypes)
 
     render() {
-        const { field, name, className, autoFocus, type, ...otherProps } = this.props;
-        const label = this.props.label || titleize(name);
+        const {
+            name, className, autoFocus, type, children, label,
+            fields: { [`${name}`]: field },
+            ...otherProps
+        } = getColumnProps(this.props);
+
         return (
-            <Col {...otherProps} className={classnames('form-field', className)}>
-                <Field label={label} error={get(field, 'failProps.errorText')}>
+            <div className={classnames('form-field', className)}>
+                <Field
+                    label={label || titleize(name)}
+                    error={get(field, 'failProps.errorText')}
+                >
                     <TextInput
                         name={name}
                         type={type}
                         autoFocus={autoFocus}
                         value={field.value}
+                        {...otherProps}
                         {...field.events}
                         onDOMChange={field.events.onChange}
                     />
+                    {children}
                 </Field>
-            </Col>
+            </div>
         );
     }
 }
