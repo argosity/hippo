0b632559d29d6f7b87e464bb7bc147c80e426f86
Nathan Stitt <nathan@stitt.org>
2017-03-15 18:56:27 -0500

    Add track_modifications migration helper

--- lib/lanes/db/migrations.rb
+++ lib/lanes/db/migrations.rb
@@ -8,6 +8,25 @@ module Lanes
             self.paths = [
               Pathname.new(__FILE__).dirname.join("../../../db/migrate").realpath
             ]
+
+            # Migration table definition helpers methods
+            module TableDefinitionHelpers
+                # track user modifications
+                def track_modifications(create_only: false)
+                    column(:created_at, :datetime,   null: false)
+                    column(:created_by_id, :integer, null: false)
+                    return if create_only
+
+                    column(:updated_at, :datetime,   null: false)
+                    column(:updated_by_id, :integer, null: false)
+                end
+            end
         end
     end
+end
+
+
+
+ActiveRecord::ConnectionAdapters::PostgreSQL::ColumnMethods.class_eval do
+    include ::Lanes::DB::Migrations::TableDefinitionHelpers
 end
