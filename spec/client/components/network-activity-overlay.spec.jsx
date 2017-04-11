617052eb649ffc727aa5ae4dc47c6f13eba444ce
Nathan Stitt <nathan@stitt.org>
2017-04-11 17:12:57 -0500

    network activity overlay

---
+++ spec/client/components/network-activity-overlay.spec.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import NetworkActivityOverlay from 'lanes/components/network-activity-overlay';
+import { Box } from '../test-models';
+import { Snapshot } from 'lanes/testing/screens';
+
+jest.useFakeTimers();
+
+describe('Network Activity Overlay', () => {
+    let model;
+    beforeEach(() => {
+        model = new Box();
+    });
+
+    it('renders', () => {
+        const activity = mount(<NetworkActivityOverlay model={model} />);
+        expect(activity).not.toHaveRendered('.mask');
+        model.syncInProgress = { method: 'PUT' };
+        expect(activity).toHaveRendered('.mask');
+        expect(activity.find('span').text()).toEqual('Saving…');
+        jest.runAllTimers();
+        expect(activity).not.toHaveRendered('.mask');
+    });
+
+    it('matches snapshot', () => {
+        model.syncInProgress = { method: 'DELETE' };
+        jest.runAllTimers();
+        expect(Snapshot(<NetworkActivityOverlay model={model} />)).toMatchSnapshot();
+    });
+});
