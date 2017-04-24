355e7078c45a792782d83b91a3c63a9407539ec2
Nathan Stitt <nathan@stitt.org>
2017-04-23 22:08:43 -0500

    remove lanes_ prefix

--- db/migrate/20140615031600_create_lanes_users.rb
+++ db/migrate/20140615031600_create_lanes_users.rb
@@ -1,12 +1,12 @@
 class CreateLanesUsers < ActiveRecord::Migration
     def change
-        create_table "lanes_users" do | t |
+        create_table :users do | t |
             t.string :login, :name, :email,  null: false
             t.string :password_digest, null: false
             t.string  "role_names",   array: true, null:false, default: []
             t.jsonb   "options",      null: false, default:  {}
             t.timestamps null:false
         end
-        add_index  :lanes_users, :role_names, using: 'gin'
+        add_index  :users, :role_names, using: 'gin'
     end
 end
