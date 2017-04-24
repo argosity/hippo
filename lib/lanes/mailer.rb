84d5736194390c6d29250f9245c915c11980e608
Nathan Stitt <nathan@stitt.org>
2017-04-23 22:35:39 -0500

    remove caching configurations

--- lib/lanes/mailer.rb
+++ lib/lanes/mailer.rb
@@ -1,40 +1,28 @@
 require 'mail'
+
 module Lanes
 
     module Mailer
-        @@configured = false
 
-        def self.set_defaults(settings = SystemSettings.for_ext(:lanes))
-            smtp = settings['smtp'] || {}
-            method = Lanes.env.production? ? :smtp : :test
-            Mail.defaults do
-                delivery_method(method, {
-                                    user_name: smtp['login'],
-                                    password:  smtp['password'],
-                                    address:   smtp['server']
-                                })
-
+        def self.create(args = {})
+            config = SystemSettings.for_ext(:lanes)['smtp'] || {}
+            delivery = delivery_method_config(config)
+            Mail::Message.new(args) do
+                from "\"#{config['from_name']}\" <#{config['from_email']}>"
+                delivery_method delivery
             end
-            @@configured = true
         end
 
-        Lanes::SystemSettings.on_change(:lanes, call_now: true) do |settings|
-            self.set_defaults(settings)
+        def self.delivery_method_config(config)
+            return :test unless Lanes.env.production?
+            {
+                smtp: {
+                    address: config['server'],
+                    user_name: config['login'],
+                    password: config['password']
+                }
+            }
         end
 
-        def self.create(args = {}, &block)
-            args[:from] = SystemSettings.for_ext(:lanes).dig('smtp', 'from')
-            set_defaults unless @@configured
-            Mail::Message.new(args, &block)
-        end
-
-        def self.deliver(args = {}, &block)
-            args[:from] = SystemSettings.for_ext(:lanes).dig('smtp', 'from')
-            set_defaults unless @@configured
-            mail = self.new(args, &block)
-            mail.deliver
-            mail
-        end
     end
-
 end
