c7b00aade7b90ce1a0fb8ae8ce8d9a270012fa78
Nathan Stitt <nathan@stitt.org>
2017-04-23 22:11:16 -0500

    add configuration_id

--- db/migrate/01_create_system_settings.rb
+++ db/migrate/01_create_system_settings.rb
@@ -2,6 +2,7 @@ class CreateSystemSettings < ActiveRecord::Migration[4.2]
     def change
 
         create_table "system_settings" do |t|
+            t.integer :configuration_id
             t.jsonb  "settings", null: false, default: {}
         end
 
