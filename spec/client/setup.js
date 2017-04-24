e8dacdd8c29543ad57833215ace16e717460ef42
Nathan Stitt <nathan@stitt.org>
2017-04-23 22:40:47 -0500

    include testing for it's mocks

--- spec/client/setup.js
+++ spec/client/setup.js
@@ -1,5 +1,6 @@
 // Code included here will be ran to setup your specs.
 // it's a usefull spot to setup globals or custom matchers
+require('lanes/testing');
 const matchers = require('lanes/testing/matchers');
 const { shallow, mount } = require('enzyme');
 
