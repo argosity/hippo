e20aaa4bd96e8fecb0a9b573bf13dd7999ef102e
Nathan Stitt <nathan@stitt.org>
2017-04-23 22:29:50 -0500

    method to get path for variant

--- lib/lanes/asset.rb
+++ lib/lanes/asset.rb
@@ -9,6 +9,10 @@ module Lanes
         def present?
             super && file.present?
         end
+
+        def path(size)
+            Lanes.config.api_path + Lanes.config.assets_path_prefix + '/' + file_data[size]['id']
+        end
     end
 
 end
