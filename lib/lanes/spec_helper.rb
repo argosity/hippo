6e6df5de16f9b0c3761ff8b3d247bab033bb6b2e
Nathan Stitt <nathan@stitt.org>
2017-04-24 20:53:18 -0500

    VCR cassette directory should go inside spec/

--- lib/lanes/spec_helper.rb
+++ lib/lanes/spec_helper.rb
@@ -13,7 +13,7 @@ require "shrine/storage/memory"
 require 'vcr'
 
 VCR.configure do |config|
-    config.cassette_library_dir = "fixtures/vcr_cassettes"
+    config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
     config.hook_into :webmock
     config.configure_rspec_metadata!
 end
