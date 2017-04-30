cfdb2730e7c35d7a27946f75c4c2afa4d6f59123
Nathan Stitt <nathan@stitt.org>
2017-04-30 15:39:10 -0500

    set model directly

--- client/lanes/models/system-setting.js
+++ client/lanes/models/system-setting.js
@@ -4,14 +4,16 @@ import {
 } from './base';
 import Sync  from './sync';
 
+import Asset from './asset';
+
 @identifiedBy('lanes/system-settings')
 export default class SystemSettings extends BaseModel {
 
     @identifier id;
     @field({ type: 'object' }) settings;
 
-    @belongsTo({ model: 'lanes/asset', inverseOf: 'owner' }) logo;
-    @belongsTo({ model: 'lanes/asset', inverseOf: 'owner' }) print_logo;
+    @belongsTo({ model: Asset, inverseOf: 'owner' }) logo;
+    @belongsTo({ model: Asset, inverseOf: 'owner' }) print_logo;
 
     fetch(options = {}) {
         return Sync.forModel(this, merge(options, { limit: 1, method: 'GET' }));
