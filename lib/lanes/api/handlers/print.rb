18b3bb414c88b103b3780aaef20cfc2e7aa81813
Nathan Stitt <nathan@stitt.org>
2017-05-06 23:12:57 -0500

    return not found when template doesn't exist

--- lib/lanes/api/handlers/print.rb
+++ lib/lanes/api/handlers/print.rb
@@ -4,6 +4,8 @@ module Lanes::API::Handlers
         def self.getter
             lambda do
                 template_klass = Lanes::Templates::Latex.for_identifier(params['template_id'])
+                status 404 and return if not template_klass
+
                 form = template_klass.new(params['model_id'])
                 content_type 'application/pdf'
                 form.as_pdf
