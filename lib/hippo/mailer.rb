77937306e4a6e2541ee4ce35ee8ef60f8ce6d2d1
Nathan Stitt <nathan@stitt.org>
2017-05-07 22:51:20 -0500

    delivery_method takes two parts

--- lib/hippo/mailer.rb
+++ lib/hippo/mailer.rb
@@ -9,14 +9,14 @@ module Hippo
             delivery = delivery_method_config(config)
             Mail::Message.new do
                 from "\"#{config['from_name']}\" <#{config['from_email']}>"
-                delivery_method delivery
+                delivery_method delivery[:via], delivery[:config]
             end
         end
 
         def self.delivery_method_config(config)
-            return :test unless Hippo.env.production?
-            {
-                smtp: {
+            return {
+                via: Hippo.env.production? ? :smtp : :test,
+                config: {
                     address: config['server'],
                     user_name: config['login'],
                     password: config['password']
