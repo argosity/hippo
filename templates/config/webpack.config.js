8ade33cee1d2df6ec1cc55cad14fe76a045e1214
Nathan Stitt <nathan@stitt.org>
2017-06-02 08:13:28 -0500

    Add preset and compression plugins

--- templates/config/webpack.config.js
+++ templates/config/webpack.config.js
@@ -1,26 +1,46 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CompressionPlugin = require("compression-webpack-plugin");
 const webpack = require('webpack');
 const path = require('path');
 
+const entries = {
+    app: [
+        '<%= "#{Hippo::Extensions.controlling.identifier}/index.js" %>',
+    ],
+};
+
+<% unless Hippo.env.production? -%>
+    for (var key in entries) {
+        entries[key].unshift('react-hot-loader/patch');
+    }
+<% end -%>
+
 const config = {
-    entry: {
-        app: [
-            'react-hot-loader/patch',
-            '<%%= "#{Hippo::Extensions.controlling.identifier}/index.js" %>',
-        ],
-    },
+    entry: entries,
     output: {
-        path: __dirname,
-        publicPath: '/',
-        filename: `${process.env.EXTENSION_ID}/.js`,
+        path: '<%= config_directory.join('..','public', 'assets') %>',
+        publicPath: '<%= Hippo.env.production? ? "https://assets.#{Hippo.config.website_domain}/assets/" : 'http://test.hippo.dev:8889/assets/' %>',
+        filename: '[name]-[hash].js',
     },
     resolve: {
-        modules: process.env.HIPPO_MODULES.split(':'),
+        modules: [
+            "<%= Hippo::Extensions.client_module_paths.join('","') %>",
+            "<%= generated_directory.to_s %>",
+        ],
         extensions: ['.js', '.jsx'],
     },
     module: {
         rules: [
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader',  'css-loader' ]
+            },
+            {
+                test: /\.(jpg|png|svg)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 25000,
+                },
+            },
             {
                 loader: 'babel-loader',
                 test: /\.jsx?$/,
@@ -28,6 +48,7 @@ const config = {
                 options: {
                     plugins: [
                         'react-hot-loader/babel',
+                        'babel-plugin-lodash',
                         'babel-plugin-transform-decorators-legacy',
                         'babel-plugin-transform-class-properties',
                         'babel-plugin-transform-function-bind',
@@ -45,6 +66,7 @@ const config = {
                 use: [
                     'style-loader',
                     'css-loader',
+                    'resolve-url-loader',
                     {
                         loader: 'sass-loader',
                         options: {
@@ -57,17 +79,23 @@ const config = {
     },
     devtool: 'source-map',
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor', minChunks: Infinity, filename: '[name].[hash].js',
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: '<%%= directory.join('index.html') %>',
-            chunks: ['app'],
-        }),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
         }),
+<% if Hippo.env.production? %>
+        new webpack.optimize.UglifyJsPlugin(), //minify everything
+        new webpack.optimize.AggressiveMergingPlugin(), //Merge chunks
+        new webpack.optimize.OccurrenceOrderPlugin(), // use smallest id for most used chuncks
+        new CompressionPlugin({
+          asset: "[path].gz[query]",
+          algorithm: "gzip",
+          test: /\.(js|html)$/,
+          threshold: 10240,
+          minRatio: 0.8
+        }),
+<% else %>
+        new webpack.NamedModulesPlugin(),
+<% end %>
     ],
     node: {
         fs: 'empty',
@@ -76,11 +104,16 @@ const config = {
         hot: true,
         inline: true,
         port: 8889,
-        historyApiFallback: true,
-        proxy: [{
-            context: '/api',
-            target: 'http://localhost:9292',
-        }],
+        contentBase: './public',
+        historyApiFallback: {
+            index: '/assets/app.html'
+        },
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+        },
+        disableHostCheck: true,
         stats: {
             colors: true,
             profile: true,
