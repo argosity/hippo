772c4ce07dae9a5316767683a8618c65ae9cda40
Nathan Stitt <nathan@stitt.org>
2017-07-31 09:41:35 -0500

    update mobx; add DateRange that uses it

---
+++ client/hippo/lib/date-range.js
@@ -0,0 +1,31 @@
+import { isString, isEmpty, map } from 'lodash';
+import { readonly } from 'core-decorators';
+import {
+    identifiedBy,
+} from 'mobx-decorated-models';
+
+@identifiedBy('date-range')
+export default class DateRange {
+
+    constructor(range) {
+        if (isString(range) && !isEmpty(range)) {
+            [this.start, this.end] = map(range.split('..'), d => new Date(d));
+        } else if (range) {
+            this.start = range.start;
+            this.end = range.end;
+        }
+    }
+
+    toJSON() {
+        if (this.start && this.end) {
+            // this strange format is what PG user and is therefore what active record expects
+            return `[${this.start.toISOString()},${this.end.toISOString()})`;
+        }
+        return '';
+    }
+
+    static serialize(range) {
+        return (range ? range.toJSON() : '');
+    }
+
+}
