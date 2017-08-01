24bb3fa9e799b665215db8ca12bb72c65051a5b1
Nathan Stitt <nathan@stitt.org>
2017-07-31 20:47:40 -0500

    mock api_host

--- spec/client/models/pub_sub.spec.js
+++ spec/client/models/pub_sub.spec.js
@@ -2,14 +2,17 @@ import { onBoot, observePubSub } from 'hippo/models/pub_sub';
 import PubSubChannel from 'hippo/models/pub_sub/channel';
 
 import { Ship, Container } from '../test-models';
-
+import Config from 'hippo/config';
 jest.mock('hippo/user', () => ({
     isLoggedIn: true,
 }));
 jest.mock('hippo/models/pub_sub/channel');
+jest.mock('hippo/config');
 
 describe('PubSub', () => {
+
     it('watches and checks in and out', () => {
+        Config.api_host = 'testing';
         const ship = new Ship();
         const container = new Container({ id: '2' });
         onBoot();
