21a3304364b0ff4ed6eea390935e567a03c8b501
Nathan Stitt <nathan@stitt.org>
2017-08-27 14:35:02 -0500

    wrap icon; set "icon" class

--- client/hippo/components/icon.jsx
+++ client/hippo/components/icon.jsx
@@ -1,3 +1,12 @@
-import Icon from 'react-fontawesome';
+import React from 'react'; // eslint-disable-line no-unused-vars
+import FAIcon from 'react-fontawesome';
+import cn from 'classnames';
+
+const Icon = (props) => {
+    const { className, ...otherProps } = props;
+    return (
+        <FAIcon className={cn('icon', className)} {...otherProps} />
+    );
+};
 
 export default Icon;
