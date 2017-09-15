6da8890f31009fd0b5e486c5feb91295c9b4130f
Nathan Stitt <nathan@stitt.org>
2017-09-15 10:10:14 -0500

    make logger a class so it can be used elsewhere

--- lib/hippo/logger.rb
+++ lib/hippo/logger.rb
@@ -3,27 +3,40 @@ require 'logger'
 
 module Hippo
 
-    class MultiDestinationLogger
+    class MultiDestinationLogger < ::Logger
+        class Output
+            def initialize
+                @targets = [STDOUT]
+                @targets.push(
+                    File.open("log/#{Hippo.config.environment}.log", "a")
+                ) if File.writable?("log")
+            end
+
+            def write(*args)
+                @targets.each {|t| t.write(*args)}
+            end
+
+            def close
+                @targets.each(&:close)
+            end
+        end
+
         def initialize
-            @targets = [STDOUT]
-            @targets.push(
-                File.open("log/#{Hippo.config.environment}.log", "a")
-            ) if File.writable?("log")
+            super Output.new
+            self.formatter = proc do |severity, datetime, progname, msg|
+                sprintf "%5.5s %s\n", severity, msg
+            end
+            if ENV['LOG']
+                self.level = ::Logger.const_get(ENV['LOG'].upcase)
+            else
+                self.level = ::Logger::INFO
+            end
         end
-
-        def write(*args)
-            @targets.each {|t| t.write(*args)}
-        end
-
-        def close
-            @targets.each(&:close)
-        end
-
     end
 
     class << self
         def logger
-            @logger ||= _create_logger
+            @logger ||= MultiDestinationLogger.new
         end
 
         def logger=( logger )
@@ -46,19 +59,5 @@ module Hippo
             logger.debug '⚡ '*40
         end
 
-        private
-
-        def _create_logger
-            logger = ::Logger.new MultiDestinationLogger.new
-            logger.formatter = proc do |severity, datetime, progname, msg|
-                sprintf "%5.5s %s\n", severity, msg
-            end
-            if ENV['LOG']
-                logger.level = ::Logger.const_get(ENV['LOG'].upcase)
-            else
-                logger.level = ::Logger::INFO
-            end
-            logger
-        end
     end
 end
