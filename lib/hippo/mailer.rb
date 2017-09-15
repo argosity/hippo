7e1d8225cd9005fc95ea5fb9351c3571dfe05779
Nathan Stitt <nathan@stitt.org>
2017-09-15 10:10:03 -0500

    get config from system

--- lib/hippo/mailer.rb
+++ lib/hippo/mailer.rb
@@ -3,29 +3,40 @@ require 'mail'
 module Hippo
 
     module Mailer
+
+        class TestMailer < Mail::TestMailer
+            def deliver!(mail)
+                retval = super
+                Hippo.logger.info { mail.encoded }
+                retval
+            end
+        end
+
         class << self
 
             def create
                 config = SystemSettings.for_ext(:smtp)
-                delivery = delivery_method_config(config)
                 Mail::Message.new do
                     from "\"#{config['from_name']}\" <#{config['from_email']}>"
-                    delivery_method delivery[:via], delivery[:config]
+                    if Hippo.env.production?
+                        delivery_method :smtp, delivery_method_config(config)
+                    else
+                        delivery_method TestMailer
+                    end
                 end
             end
 
             def delivery_method_config(config)
-                config = Hippo.config.secrets.smtp || {}
-                {
-                    via: Hippo.env.production? ? :smtp : :test,
-                    config: {
+                Hippo::Tenant.system.perform do
+                    config = Hippo.config.secrets.smtp || {}
+                    {
                         address: config['address'],
                         user_name: config['user_name'],
                         password: config['password'],
                         enable_starttls_auto: true,
                         port: 587
                     }
-                }
+                end
             end
 
             def from_template(template)
