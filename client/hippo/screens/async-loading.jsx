e16f5381662c2ece6083bd945e44ec22e3952792
Nathan Stitt <nathan@stitt.org>
2017-11-24 20:23:41 -0600

    add retry button for failure loading

--- client/hippo/screens/async-loading.jsx
+++ client/hippo/screens/async-loading.jsx
@@ -1,19 +1,27 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import Spinning from 'grommet/components/icons/Spinning';
+import FormRefreshIcon from 'grommet/components/icons/base/FormRefresh';
 import AlertIcon from 'grommet/components/icons/base/Alert';
+import Button from 'grommet/components/Button';
 import { asyncComponent } from 'react-async-component';
 import cn from 'classnames';
 
-const ErrorComponent = ({ screen }) => (
+const ErrorComponent = ({ screen, retry }) => (
     <div
         data-screen-id={screen.definition.id}
         className={cn('async-loading', 'error', { 'is-active': screen.isActive })}
     >
         <div className="content">
-            <AlertIcon /> Failed to load {screen.definition.title}…
+            <h4><AlertIcon /> Failed to load {screen.definition.title}…</h4>
+            <Button
+                icon={<FormRefreshIcon />}
+                onClick={retry}
+                label="Retry"
+            />
         </div>
     </div>
 );
+
 
 const LoadingComponent = ({ screen }) => (
     <div
@@ -21,7 +29,7 @@ const LoadingComponent = ({ screen }) => (
         className={cn('async-loading', 'is-loading', { 'is-active': screen.isActive })}
     >
         <div className="content">
-            <Spinning /> Loading {screen.definition.title}…
+            <h4><Spinning /> Loading {screen.definition.title}…</h4>
         </div>
     </div>
 );
