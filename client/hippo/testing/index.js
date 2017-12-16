62fb8c4b799509af936581e325ce82d606f4f374
Nathan Stitt <nathan@stitt.org>
2017-12-15 22:39:43 -0600

    setup a subscription when testing

--- client/hippo/testing/index.js
+++ client/hippo/testing/index.js
@@ -1,7 +1,14 @@
 /* global jest */
+import Tenant from 'hippo/models/tenant';
+import Config from 'hippo/config';
+import Subscription from 'hippo/models/subscription';
+
 export React from 'react';
 
 export { getScreenInstance, Snapshot } from './screens';
+
+Config.subscription_plans = [new Subscription({ id: 1, price: '42.42' })];
+Tenant.current.subscription_id = Subscription.all[0].id;
 
 jest.mock('hippo/config');
 jest.mock('hippo/models/sync');
