e20c7c9b65beb1e55b2dfe9b4ebe70544ee9181f
Nathan Stitt <nathan@stitt.org>
2017-12-30 23:44:47 -0600

    pass props onto toolbar header

--- client/hippo/components/toolbar.jsx
+++ client/hippo/components/toolbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import Header from 'grommet/components/Header';
+import cn from 'classnames';
 
 export SaveButton from './save-button';
 
-export function Toolbar({ children }) {
+export function Toolbar({ children, className, ...props }) {
     return (
         <Header
-            className="hippo-toolbar"
-            fixed colorIndex="light-2" align="center"
+            align="center"
+            fixed colorIndex="light-2"
+            className={cn('hippo-toolbar', className)}
             pad={{ horizontal: 'small', vertical: 'small', between: 'small' }}
+            {...props}
         >
             {children}
         </Header>
