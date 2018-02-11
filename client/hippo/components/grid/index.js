fd99afc224181f79b5f770eba13f7f5a0b67ecf6
Nathan Stitt <nathan@stitt.org>
2018-02-10 18:42:00 -0600

    extract media queries to own file

---
+++ client/hippo/components/grid/index.js
@@ -0,0 +1,10 @@
+import { Grid, Cell as StyledCell } from 'styled-css-grid';
+import media from './media';
+
+const Cell = StyledCell.extend`
+${props => props.desktop && media.desktop`grid-column-end: span ${props.desktop};`}
+${props => props.tablet && media.tablet`grid-column-end: span ${props.tablet};`}
+${props => props.phone && media.phone`grid-column-end: span ${props.phone};`}
+`;
+
+export { Cell, Grid };
--- client/hippo/components/grid/media.js
+++ client/hippo/components/grid/media.js
@@ -1,5 +1,4 @@
 import { css } from 'styled-components';
-import { Grid, Cell as StyledCell } from 'styled-css-grid';
 
 const SIZES = {
     desktop: 992,
@@ -20,10 +19,4 @@ const media = SIZE_KEYS.reduce((acc, label) => {
     return acc;
 }, {});
 
-const Cell = StyledCell.extend`
-${props => props.desktop && media.desktop`grid-column-end: span ${props.desktop};`}
-${props => props.tablet && media.tablet`grid-column-end: span ${props.tablet};`}
-${props => props.phone && media.phone`grid-column-end: span ${props.phone};`}
-`;
-
-export { Cell, Grid };
+export default media;
