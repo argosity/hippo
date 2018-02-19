37367a65b23b0eb4885512020abc605fa2d7b943
Nathan Stitt <nathan@stitt.org>
2018-02-19 09:58:42 -0600

    add a footer

---
+++ client/hippo/components/footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
+import { Toolbar, SaveButton } from './toolbar';
+
+export { SaveButton };
+
+export function Footer({ children, ...props }) {
+    return (
+        <Toolbar
+            gridArea="footer"
+            align="end"
+            justify="end"
+            direction="row"
+            {...props}
+        >
+            {children}
+        </Toolbar>
+    );
+}
