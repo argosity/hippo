00a593ce110c939fe46df778ec5b54d9ab3ac433
Nathan Stitt <nathan@stitt.org>
2018-03-25 20:59:38 -0500

    use grid for layout

--- client/hippo/screens/user-management/edit-form.jsx
+++ client/hippo/screens/user-management/edit-form.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Row, Col } from 'react-flexbox-grid';
-
 import { observer }   from 'mobx-react';
 import { observable, action } from 'mobx';
-
-import Box    from 'grommet/components/Box';
-import Button from 'grommet/components/Button';
+import { Button } from 'grommet';
 import Warning from '../../components/warning-notification';
 import SaveButton from '../../components/save-button';
 import Query from '../../models/query';
-
+import { Cell } from '../../components/grid';
 import { Form, Field, FormState, nonBlank, validEmail, booleanValue } from '../../components/form';
 
 @observer
@@ -58,28 +54,24 @@ export default class EditForm extends React.Component {
     render() {
         return (
             <Form
-                tag="div"
+                grid
                 ref="form"
                 model={this.user}
                 className="user-edit-form"
             >
                 <Warning message={this.errorMessage} />
-                <Row middle='sm'>
-                    <Field md={4} xs={6} name="login" validate={nonBlank} />
-                    <Field md={4} xs={6} name="name" validate={nonBlank} />
-                    <Field md={4} xs={6} name="email" validate={validEmail} />
-                    <Field md={4} xs={6} type="password" name="password" />
-                    <Field md={4} xs={6} type="checkbox" name="is_admin" validate={booleanValue} />
-                    <Col md={4} xs={6}>
-                        <Box direction="row" justify="between">
-                            <Button label="Cancel" onClick={this.onCancel} accent />
-                            <SaveButton
-                                onClick={this.formState.isValid ? this.onSave : null}
-                                model={this.user}
-                            />
-                        </Box>
-                    </Col>
-                </Row>
+                <Field phone={1} name="login" validate={nonBlank} />
+                <Field phone={2} name="name" validate={nonBlank} />
+                <Field phone={2} name="email" validate={validEmail} />
+                <Field phone={1} type="password" name="password" />
+                <Field phone={1} align="center" type="checkbox" name="is_admin" validate={booleanValue} />
+                <Cell phone={3} center middle gap="small">
+                    <Button label="Cancel" onClick={this.onCancel} accent />
+                    <SaveButton
+                        onClick={this.formState.isValid ? this.onSave : null}
+                        model={this.user}
+                    />
+                </Cell>
             </Form>
         );
     }
