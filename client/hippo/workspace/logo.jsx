0469ea67f58270c7817a869ac7d8fe8af2b786be
Nathan Stitt <nathan@stitt.org>
2018-03-25 20:58:13 -0500

    Display tenant name if logo is missing

--- client/hippo/workspace/logo.jsx
+++ client/hippo/workspace/logo.jsx
@@ -1,14 +1,21 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import { observer } from 'mobx-react';
 import { get, isEmpty } from 'lodash';
-import { Box } from 'grommet';
+import { Box, Heading } from 'grommet';
+import Tenant from '../models/tenant';
 import Config from '../config';
 import Asset from '../models/asset';
 
 const Logo = observer(() => {
     if (!get(Config, 'logo.thumbnail')) {
         if (!isEmpty(Config.product_name)) {
-            return <Box margin="none" className="product-name">{Config.product_name}</Box>;
+            return (
+                <Box margin="small">
+                    <Heading level={3} margin="none" className="product-name">
+                        {Tenant.current.name}
+                    </Heading>
+                </Box>
+            );
         }
         return null;
     }
