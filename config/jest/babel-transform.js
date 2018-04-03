81213c2092b3b79734a302bf690ee3a53eb9ddbe
Nathan Stitt <nathan@stitt.org>
2018-04-02 20:59:09 -0500

    update libs and snapshots

--- config/jest/babel-transform.js
+++ config/jest/babel-transform.js
@@ -1,6 +1,6 @@
 const babelOptions = {
     plugins: [
-        'react-hot-loader/babel',
+        'babel-plugin-dynamic-import-node',
         'babel-plugin-transform-decorators-legacy',
         'babel-plugin-transform-class-properties',
         'babel-plugin-transform-function-bind',
